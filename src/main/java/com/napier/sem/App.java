7c1c3beca389df2504af8bf3d5dcd47817020cc6
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2021-03-02 15:06:37 +0000

    Adjust ahead of code review

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -29,15 +29,18 @@ public class App {
         // dao.testQuery();
 
         // Use-case 1.1
-        // produce a report on all countries in the world organised by largest population to smallest
+        // Produce a report on all countries in the world organised by largest population to smallest
         // ArrayList<Country> countries = dao.allCountries();
 
         // Use-cases 1.2 and 1.3
-        // produce a report on all countries in the world organised by largest population to smallest
-        // ArrayList<Country> countries = dao.allCountries("region", "Southern and Central Asia");
+        // Produce a report on all countries in a continent organised by largest population to smallest
+        // ArrayList<Country> countries = dao.allCountries(app.CONTINENT, "Africa")
+        // Produce a report on all countries in a region organised by largest population to smallest
+        // ArrayList<Country> countries = dao.allCountries(app.REGION, "Southern and Central Asia");
+
 
         // Use-case 2.1
-        // Run top 10 countries query against database
+        // Run top N countries query against database
         // ArrayList<Country> countries = dao.TopNCountries(10);
 
         // Use-case 2.2
@@ -49,17 +52,21 @@ public class App {
         // ArrayList<Country> countries = dao.TopNCountriesRegion(5,"Eastern Asia");
 
         // Use-case 3.1
-        // produce a report on all cities in the world organised by largest population to smallest
+        // Produce a report on all cities in the world organised by largest population to smallest
         ArrayList<City> cities = dao.allCities();
 
         // Use-case 3.2 - 3.4
-        // produce a report on all cities in a defined area organised by largest population to smallest
+        // Produce a report on all cities in a continent organised by largest population to smallest
         // ArrayList<City> cities = dao.allCities(app.CONTINENT, "Europe");
+        // Produce a report on all cities in a region organised by largest population to smallest
+        // ArrayList<City> cities = dao.allCities(app.REGION, "Europe");
+        // Produce a report on all cities in a country organised by largest population to smallest
+        // ArrayList<City> cities = dao.allCities(app.COUNTRY, "Europe");
 
         // Use-case 3.5
-        // produce a report on all cities in a district organised by largest population to smallest
         // We'll leave this for next sprint, but I think some readability is being sacrificed in
         // the way we're doing this:
+        // Produce a report on all cities in a district organised by largest population to smallest
         // ArrayList<City> cities = dao.allCities("Alagoas");
 
         // Display Country results
@@ -97,7 +104,9 @@ public class App {
                 // Wait a bit for db to start
                 Thread.sleep(10000);
                 // Connect to database
-                connection = DriverManager.getConnection("jdbc:mysql://database:3306/world?useSSL=false", "root", "example");
+                connection = DriverManager.getConnection("jdbc:mysql://database:3306/world?useSSL=false",
+                        "root",
+                        "example");
                 System.out.println("Successfully connected");
                 break;
             }

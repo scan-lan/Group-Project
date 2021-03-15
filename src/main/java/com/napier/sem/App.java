787628c56cc5011aa2ad6caf71e6e6adcf80c180
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2021-03-15 21:37:11 +0000

    Refactor DAO and improve SQL queries

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -6,10 +6,11 @@ import java.util.ArrayList;
 public class App
 {
     // Constants
-    private final String CONTINENT = "continent";
-    private final String REGION = "region";
-    private final String COUNTRY = "name";
-    private final String DISTRICT = "district";
+    private static final String WORLD = "world";
+    private static final String CONTINENT = "continent";
+    private static final String REGION = "region";
+    private static final String COUNTRY = "country";
+    private static final String DISTRICT = "district";
 
     // Connection to MySQL database.
     private Connection connection = null;
@@ -24,52 +25,36 @@ public class App
         // Create instance of the database access object
         DAO dao = new DAO(app.connection);
 
-        // Run the test query
-        // dao.testQuery();
-
-        // Use-case 1.1
+        // Use-case 1.1-1.3
         // Produce a report on all countries in the world organised by largest population to smallest
-        // ArrayList<Country> countries = dao.allCountries();
-
-        // Use-cases 1.2 and 1.3
+        // ArrayList<Country> countries = dao.allCountriesIn(WORLD, "");
         // Produce a report on all countries in a continent organised by largest population to smallest
-        // ArrayList<Country> countries = dao.allCountries(app.CONTINENT, "Africa")
+        // ArrayList<Country> countries = dao.allCountriesIn(CONTINENT, "Africa");
         // Produce a report on all countries in a region organised by largest population to smallest
-        // ArrayList<Country> countries = dao.allCountries(app.REGION, "Southern and Central Asia");
+        // ArrayList<Country> countries = dao.allCountriesIn(REGION, "Southern and Central Asia");
 
-
-        // Use-case 2.1
+        // Use-case 2.1-2.3
         // Run top N countries query against database
-        // ArrayList<Country> countries = dao.TopNCountries(10);
-
-        // Use-case 2.2
+        // ArrayList<Country> countries = dao.TopNCountriesIn(WORLD, "", 10);
         // Run top N populated countries in specified continent query
-        // ArrayList<Country> countries = dao.TopNCountriesContinent(5,"Asia");
-
-        // Use-case 2.3
+        // ArrayList<Country> countries = dao.TopNCountriesIn(CONTINENT, "Asia", 5);
         // Run top N populated countries in specified region query
-        // ArrayList<Country> countries = dao.TopNCountriesRegion(5,"Eastern Asia");
+        // ArrayList<Country> countries = dao.TopNCountriesIn(REGION, "Eastern Asia", 5);
 
-        // Use-case 3.1
+        // Use-case 3.1-3.5
         // Produce a report on all cities in the world organised by largest population to smallest
-        ArrayList<City> cities = dao.allCities();
-
-        // Use-case 3.2 - 3.4
+        // ArrayList<City> cities = dao.allCitiesIn(WORLD, "");
         // Produce a report on all cities in a continent organised by largest population to smallest
-        // ArrayList<City> cities = dao.allCities(app.CONTINENT, "Europe");
+        // ArrayList<City> cities = dao.allCitiesIn(CONTINENT, "Europe");
         // Produce a report on all cities in a region organised by largest population to smallest
-        // ArrayList<City> cities = dao.allCities(app.REGION, "Europe");
+        ArrayList<City> cities = dao.allCitiesIn(REGION, "Caribbean");
         // Produce a report on all cities in a country organised by largest population to smallest
-        // ArrayList<City> cities = dao.allCities(app.COUNTRY, "Europe");
-
-        // Use-case 3.5
-        // We'll leave this for next sprint, but I think some readability is being sacrificed in
-        // the way we're doing this:
+        // ArrayList<City> cities = dao.allCitiesIn(COUNTRY, "India");
         // Produce a report on all cities in a district organised by largest population to smallest
-        // ArrayList<City> cities = dao.allCities("Alagoas");
+        // ArrayList<City> cities = dao.allCitiesIn(DISTRICT, "Alagoas");
 
         // Display Country results
-        //for (Country country : countries) System.out.println(country);
+        // for (Country country : countries) System.out.println(country);
 
         // Display City results
         for (City city : cities) System.out.println(city);
@@ -94,14 +79,14 @@ public class App
             System.exit(-1);
         }
 
-        int retries = 30;
+        int retries = 60;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
             try
             {
                 // Wait a bit for db to start
-                Thread.sleep(10000);
+                Thread.sleep(5000);
                 // Connect to database
                 connection = DriverManager.getConnection("jdbc:mysql://database:3306/world?useSSL=false",
                         "root",

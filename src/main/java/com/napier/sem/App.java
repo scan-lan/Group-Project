c87958eb799cc01412c3301f5d63e89af7f8e226
Craig Gordon <40456374@napier.ac.uk>
2021-03-16 19:08:47 +0000

    Adds capitalCities.sql Adds use-cases 5-1 - 5-3 code in the main method Adds use-cases 5-1 - 5-3 SQL/Java code to the DOA classfile Adds CapitalCity.java class file for use with these queries and Java code

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -47,7 +47,7 @@ public class App
         // Produce a report on all cities in a continent organised by largest population to smallest
         // ArrayList<City> cities = dao.allCitiesIn(CONTINENT, "Europe");
         // Produce a report on all cities in a region organised by largest population to smallest
-        ArrayList<City> cities = dao.allCitiesIn(REGION, "Caribbean");
+        // ArrayList<City> cities = dao.allCitiesIn(REGION, "Caribbean");
         // Produce a report on all cities in a country organised by largest population to smallest
         // ArrayList<City> cities = dao.allCitiesIn(COUNTRY, "India");
         // Produce a report on all cities in a district organised by largest population to smallest
@@ -68,11 +68,11 @@ public class App
 
         // Use-case 5.1-5.3
         // Produce a report on all capital cities in the world organised by largest population to smallest
-         ArrayList<CapitalCity> capitalCities = dao.allCapitalCitiesIn(WORLD, "");
+        // ArrayList<CapitalCity> capitalCities = dao.allCapitalCitiesIn(WORLD, "");
         // Produce a report on all capital cities in a continent organised by largest population to smallest
         // ArrayList<CapitalCity> capitalCities = dao.allCapitalCitiesIn(CONTINENT, "Asia");
         // Produce a report on all capital cities in a region organised by largest population to smallest
-        // ArrayList<CapitalCity> capitalCities = dao.allCapitalCitiesIn(REGION, "Caribbean");
+        ArrayList<CapitalCity> capitalCities = dao.allCapitalCitiesIn(REGION, "Caribbean");
 
         // Display Country results
         // for (Country country : countries) System.out.println(country);
--- src/main/java/com/napier/sem/DAO.java
+++ src/main/java/com/napier/sem/DAO.java
@@ -20,8 +20,7 @@ public class DAO
     private static final String COUNTRY = "country";
     private static final String DISTRICT = "district";
 
-    public DAO(Connection connection)
-    {
+    public DAO(Connection connection) {
         this.connection = connection;
     }
 
@@ -33,23 +32,18 @@ public class DAO
      * @param statementString The SQL statement to be executed
      * @return An ArrayList of country objects
      */
-    private ArrayList<Country> ExecuteCountryStatement(String statementString)
-    {
+    private ArrayList<Country> ExecuteCountryStatement(String statementString) {
         ArrayList<Country> countries = new ArrayList<>();
-        try
-        {
+        try {
             // Create the SQL statement object for sending statements to the database
             Statement statement = connection.createStatement();
             // Execute the query
             ResultSet resultSet = statement.executeQuery(statementString);
             // Create Country object and add it to the list for each result in the query
-            while (resultSet.next())
-            {
+            while (resultSet.next()) {
                 countries.add(new Country(resultSet));
             }
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             System.out.println("Query failed");
             System.out.println(e.getMessage());
         }
@@ -65,8 +59,7 @@ public class DAO
      * @param statementString The SQL statement to be executed
      * @return An ArrayList of city objects
      */
-    private ArrayList<City> ExecuteCityStatement(String statementString)
-    {
+    private ArrayList<City> ExecuteCityStatement(String statementString) {
         ArrayList<City> cities = new ArrayList<>();
         try {
             // Create the SQL statement object for sending statements to the database
@@ -77,9 +70,7 @@ public class DAO
             while (resultSet.next()) {
                 cities.add(new City(resultSet));
             }
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             System.out.println("Query ExecuteCityStatement failed");
             System.out.println(e.getMessage());
         }
@@ -120,12 +111,10 @@ public class DAO
      *
      * @return An ordered list of countries in the world sorted by descending population
      */
-    public ArrayList<Country> allCountriesIn(String area, String areaName)
-    {
+    public ArrayList<Country> allCountriesIn(String area, String areaName) {
         String whereClause;
 
-        switch (area)
-        {
+        switch (area) {
             case CONTINENT:
                 whereClause = "WHERE country.continent = '" + areaName + "'\n";
                 break;
@@ -141,7 +130,7 @@ public class DAO
         String statementString = "SELECT code, country.name, continent, region, country.population, city.name AS capital\n" +
                 "FROM country\n" +
                 "    JOIN city ON country.capital = city.id\n" +
-                whereClause + 
+                whereClause +
                 "ORDER BY country.population DESC";
 
         return ExecuteCountryStatement(statementString);
@@ -154,12 +143,10 @@ public class DAO
      *
      * @return An ordered list of countries sorted by descending population
      */
-    public ArrayList<Country> TopNCountriesIn(String area, String areaName, Integer n)
-    {
+    public ArrayList<Country> TopNCountriesIn(String area, String areaName, Integer n) {
         String whereClause;
 
-        switch (area)
-        {
+        switch (area) {
             case CONTINENT:
                 whereClause = "WHERE country.continent = '" + areaName + "'\n";
                 break;
@@ -188,12 +175,10 @@ public class DAO
      *
      * @return An ordered list of cities in a defined area sorted by descending population
      */
-    public ArrayList<City> allCitiesIn(String area, String areaName)
-    {
+    public ArrayList<City> allCitiesIn(String area, String areaName) {
         String whereClause;
 
-        switch (area)
-        {
+        switch (area) {
             case CONTINENT:
                 whereClause = "WHERE country.continent = '" + areaName + "'\n";
                 break;

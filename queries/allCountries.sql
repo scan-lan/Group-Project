fe582136157b2d69279da7672c9476c3beea3750
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2021-03-16 17:13:40 +0000

    Address PR comments

--- queries/allCountries.sql
+++ queries/allCountries.sql
@@ -1,5 +1,5 @@
 # All countries in the world organised by largest population to smallest.
-SELECT code, country.name, region, country.population, city.name
+SELECT code, country.name, region, country.population, city.name AS capital
 FROM country
     JOIN city ON country.capital = city.id
 ORDER BY country.population DESC;

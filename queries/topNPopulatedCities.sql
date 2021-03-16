ec79c384ba5ab488aaff46e09fb9277bb5cb6f32
Lukas Paling <40456378@live.napier.ac.uk>
2021-03-16 18:29:25 +0000

    Tidy up of SQL file topNPopulatedCities.sql

--- queries/topNPopulatedCities.sql
+++ queries/topNPopulatedCities.sql
@@ -1,6 +1,7 @@
-# 4.1 - 4.5
+#
+4.1 - 4.5
 # The top N populated cities in ... where N is provided by the user.
-SELECT ID, city.name, district, population, country.name AS country
+SELECT ID, city.name, district, city.population, country.name AS country
 FROM city
 JOIN country
 ON city.countrycode = country.code
@@ -10,4 +11,5 @@ ON city.countrycode = country.code
 4. WHERE country.name = ...
 5. WHERE city.district = ...
 ORDER BY population DESC
-LIMIT N; # variable
+LIMIT N; #
+variable

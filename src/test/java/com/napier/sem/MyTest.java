498a6c8d4ef91509314dcf512cb8ddb88cf61427
Craig Gordon <40456374@napier.ac.uk>
2021-03-20 16:36:40 +0000

    Addresses the first and second comments from Luke in the PR. Changes assertTrue to assertEquals in the topNCitiesInTest2  topNCitiesInTest3 integration test methods for simplicity  Changed the connection code to point to the connection variable in the main App rather than the variable called connection created in the MyTest class file

--- src/test/java/com/napier/sem/MyTest.java
+++ src/test/java/com/napier/sem/MyTest.java
@@ -9,30 +9,31 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class MyTest
-{
+public class MyTest {
     static App app;
 
     // Connection to MySQL database.
     final static Connection connection = null;
 
     // Create instance of the database access object
-    DAO dao = new DAO(MyTest.connection);
+    // For some reason this doesn't work, it has to be called from the App
+    // DAO dao = new DAO(MyTest.connection);
+    DAO dao = new DAO(App.connection);
+
 
     @BeforeAll
-    static void init()
-    {
+    static void init() {
         // Create new Application
         App app = new App();
         // Connect to database
-        app.connect("localhost:33060");
+        app.connect("localhost:33061");
     }
 
     @Test
     void topNCitiesInTest1()
     {
         Integer N = 5;
-        ArrayList<City> cities = dao.topNCitiesIn("continent","europe",N);
+        ArrayList<City> cities = dao.topNCitiesIn("continent", "europe", N);
         assertTrue(cities.size() <= N);
         assertEquals(cities.get(0).name, "Moscow");
         assertEquals(cities.get(1).name, "London");
@@ -45,7 +46,7 @@ public class MyTest
     void topNCitiesInTest2()
     {
         Integer N = 0;
-        ArrayList<City> cities = dao.topNCitiesIn("world","",N);
+        ArrayList<City> cities = dao.topNCitiesIn("world", "", N);
         assertEquals(cities.size(), 0);
     }
 

9b0e00754a706c589fa064415e8fe4cd4f8bcefd
Craig <40456374@live.napier.ac.uk>
2021-03-20 13:25:13 +0000

    Test Setup Complete

--- src/main/java/com/napier/sem/DAO.java
+++ src/main/java/com/napier/sem/DAO.java
@@ -15,7 +15,6 @@ public class DAO
 {
     // Private properties
     private final Connection connection;
-    private static final String WORLD = "";
     private static final String CONTINENT = "continent";
     private static final String REGION = "region";
     private static final String COUNTRY = "country";

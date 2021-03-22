44c84d7c40a46190864fb439653cac1628c16085
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2021-03-22 01:40:58 +0000

    Add unit tests for getWhereCondition

---
+++ src/test/java/com/napier/sem/DAO_UnitTests.java
@@ -0,0 +1,34 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DAO_UnitTests
+{
+    @Test
+    public void getWhereCondition_unknownAreaFilterReturnsNull()
+    {
+        // given
+        String areaFilter = "planet";
+
+        // when
+        String whereCondition = DAO.getWhereCondition(areaFilter, "Earth");
+
+        // then
+        assertNull(whereCondition);
+    }
+
+    @Test
+    public void getWhereCondition_areaNameAppearsInWhereCondition()
+    {
+        // given
+        String areaName = "New York";
+
+        // when
+        String whereCondition = DAO.getWhereCondition(App.DISTRICT, areaName);
+
+        // then
+        assertTrue(whereCondition.contains(areaName));
+    }
+}

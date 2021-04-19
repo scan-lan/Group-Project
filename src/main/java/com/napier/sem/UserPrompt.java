ec31487c459f6c5e8a9113404aaa3398bd01c7c7
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2021-04-20 00:07:24 +0100

    Implement full command line functionality
    Awaiting proper input validation and polishing

--- src/main/java/com/napier/sem/UserPrompt.java
+++ src/main/java/com/napier/sem/UserPrompt.java
@@ -1,8 +1,6 @@
 package com.napier.sem;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class UserPrompt
 {
@@ -10,28 +8,66 @@ public class UserPrompt
     private final Scanner scanner;
     private static final int[] topNQueryParentIds = new int[]{2, 4, 6};
     private static final String USE_CASE_PROMPT = "Enter the number corresponding to the type of query you'd like to run\n" +
-            "Your options are as follows:";
+            "Your options are as follows:\n" +
+            "1) All countries in...\n" +
+            "2) Top N countries in...\n" +
+            "3) All cities in...\n" +
+            "4) Top N cities in...\n" +
+            "5) All capital cities in...\n" +
+            "6) Top N capital cities in...\n" +
+            "7) Residence report of people living in...\n" +
+            "8) Population of...\n" +
+            "9) Report on number of speakers per language for chinese, english, hindi, spanish and arabic\n" +
+            "All results will be sorted in order of largest population to smallest\n" +
+            "Enter 'q' at any time to exit";
+    private final ArrayList<QueryInfo> queryList = new ArrayList<>();
 
     public UserPrompt(DAO dao)
     {
         this.dao = dao;
         this.scanner = new Scanner(System.in);
+
+        queryList.add(new QueryInfo(1,
+                "All countries in",
+                new String[]{"the world", "a given continent", "a given region"}));
+        queryList.add(new QueryInfo(2,
+                "Top N countries in",
+                new String[]{"the world", "a given continent", "a given region"}));
+        queryList.add(new QueryInfo(3,
+                "All cities in",
+                new String[]{"the world", "a given continent", "a given region", "a given country", "a given district"}));
+        queryList.add(new QueryInfo(4,
+                "Top N cities in",
+                new String[]{"the world", "a given continent", "a given region", "a given country", "a given district"}));
+        queryList.add(new QueryInfo(5,
+                "All capital cities in",
+                new String[]{"the world", "a given continent", "a given region"}));
+        queryList.add(new QueryInfo(6,
+                "Top N capital cities in",
+                new String[]{"the world", "a given continent", "a given region"}));
+        queryList.add(new QueryInfo(7,
+                "Report on people living in vs not in cities in",
+                new String[]{"a given continent", "a given region", "a given country"}));
+        queryList.add(new QueryInfo(8,
+                "The population of",
+                new String[]{"the world", "a given continent", "a given region", "a given country", "a given district", "a given city"}));
+        queryList.add(new QueryInfo(9,
+                "Report on the number of speakers of Chinese, English, Spanish, Hindi and Arabic worldwide",
+                new String[]{}));
     }
 
     public void enterLoop()
     {
-        System.out.println(USE_CASE_PROMPT);
+        int parentQueryChoice = obtainParentQueryChoice();
 
-        int parentQueryChoice = scanner.nextInt();
-
-        System.out.println("Choose the area you'd like to restrict the query to: 1, 2 or 3");
-
-        int childQueryChoice = scanner.nextInt();
-
-        String areaFilter = parseQueryInputForAreaFilter(parentQueryChoice, childQueryChoice);
+        String areaFilter = "";
+        if (parentQueryChoice != 9)
+        {
+            areaFilter = obtainChildQueryChoice(parentQueryChoice);
+        }
 
         String areaNameInput = "";
-        if (areaFilter != App.WORLD)
+        if (parentQueryChoice != 9 && areaFilter != App.WORLD)
         {
             System.out.println("Enter the area you'd like to query");
 
@@ -47,6 +83,66 @@ public class UserPrompt
         }
 
         executeQueryFromInput(parentQueryChoice, areaFilter, areaNameInput, n);
+    }
+
+    private int obtainParentQueryChoice()
+    {
+        int parentQueryChoice = -1;
+
+        while (parentQueryChoice < 1)
+        {
+            System.out.println(USE_CASE_PROMPT);
+            Integer input = obtainValidIntWithinRange(queryList.size());
+            if (input == null)
+            {
+                System.out.println("Sorry, that's not a valid choice. Pick a number from 1 to " + queryList.size());
+            }
+            else
+            {
+                parentQueryChoice = input;
+            }
+        }
+        return parentQueryChoice;
+    }
+
+    private String obtainChildQueryChoice(int parentQuery)
+    {
+        if (parentQuery == 9) return "";
+
+        QueryInfo queryInfo = queryList.get(parentQuery - 1);
+        String[] childQueries = queryInfo.getChildQueries();
+
+        String childQueryPrompt = String.format("Choose the type of \"%s\" query you'd like to run.\n", queryInfo.getParentQuery());
+        for (int i = 0; i < queryInfo.getChildQueries().length; i++)
+        {
+            childQueryPrompt += String.format("%d) %s %s\n", i+1, queryInfo.getParentQuery(), childQueries[i]);
+        }
+
+        int childQuery = -1;
+        while (childQuery < 1)
+        {
+            System.out.println(childQueryPrompt);
+            Integer input = obtainValidIntWithinRange(childQueries.length);
+            if (input == null)
+            {
+                System.out.println("Sorry, that's not a valid choice. Pick a number from 1 to " + childQueries.length);
+            }
+            else
+            {
+                childQuery = input;
+            }
+        }
+
+        return parseQueryInputForAreaFilter(parentQuery, childQuery);
+    }
+
+    private Integer obtainValidIntWithinRange(int maxNum)
+    {
+        int input = scanner.nextInt();
+
+        if (input < 1 || input > maxNum) return null;
+
+        return input;
     }
 
     private String parseQueryInputForAreaFilter(int parentQueryChoice, int areaFilterChoice)
@@ -84,6 +180,27 @@ public class UserPrompt
             case (2):
                 records = dao.topNCountriesIn(areaFilter, areaName, n);
                 break;
+            case (3):
+                records = dao.allCitiesIn(areaFilter, areaName);
+                break;
+            case (4):
+                records = dao.topNCitiesIn(areaFilter, areaName, n);
+                break;
+            case (5):
+                records = dao.allCapitalCitiesIn(areaFilter, areaName);
+                break;
+            case (6):
+                records = dao.topNCapitalCitiesIn(areaFilter, areaName, n);
+                break;
+            case (7):
+                records = dao.populationLivingInAndNotInCities(areaFilter, areaName);
+                break;
+            case (8):
+                records = dao.populationOf(areaFilter, areaName);
+                break;
+            case (9):
+                records = dao.languageReport();
+                break;
             default:
                 records = new ArrayList<>();
                 break;
@@ -96,16 +213,20 @@ public class UserPrompt
     }
 }
 
-//class ParentQuery
-//{
-//    private final int parentID;
-//    private final String description;
-//    private final String[] childQueries;
-//
-//    public ParentQuery(int id, String description, String[] childQueries)
-//    {
-//        parentID = id;
-//        this.description = description;
-//        this.childQueries = childQueries;
-//    }
-//}
+class QueryInfo
+{
+    private final int queryId;
+    private final String parentQuery;
+    private final String[] childQueries;
+
+    public QueryInfo(int queryId, String parentQuery, String[] childQueries)
+    {
+        this.queryId = queryId;
+        this.parentQuery = parentQuery;
+        this.childQueries = childQueries;
+    }
+
+    public int getQueryId() { return queryId; }
+    public String getParentQuery() { return parentQuery; }
+    public String[] getChildQueries() { return childQueries; }
+}

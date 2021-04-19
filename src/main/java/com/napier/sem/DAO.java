d291246b0d3f9cfe4b3dc635d1880e026c3c4794
Luke Scanlan <luke.scanlan@postcodelottery.co.uk>
2021-04-19 22:24:51 +0100

    Make executeQuery and getWhere methods private

--- src/main/java/com/napier/sem/DAO.java
+++ src/main/java/com/napier/sem/DAO.java
@@ -24,7 +24,7 @@ public class DAO
      * @param areaName The name of the location which will be used
      * @return The where condition string
      */
-    public static String getWhereCondition(String areaFilter, String areaName)
+    private static String getWhereCondition(String areaFilter, String areaName)
     {
         String whereCondition;
 
@@ -63,7 +63,7 @@ public class DAO
      * @param statementString The SQL statement to be executed
      * @return An ArrayList of country objects
      */
-    public ArrayList<Record> executeStatement(String statementString, String recordType)
+    private ArrayList<Record> executeStatement(String statementString, String recordType)
     {
         ArrayList<Record> records = new ArrayList<>();
 
